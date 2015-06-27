ef0c245e125db95071dd36597201d67a1a527c15
MR.Y <programming@yatsukouji.com>
2015-06-27 20:34:04 +0900

    reviewコントローラを編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,18 @@
 class ReviewsController < ApplicationController
-  def create
-
+   def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to review_url(@review.restaurant_id)
+    else
+      redirect_to review_url(@review.restaurant_id)
+    end
   end
 
-  def destroy
 
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body, :restaurant_id)
   end
 end
