e1695a3751464f165991fa1d2cee75efd72bc4e3
MR.Y <programming@yatsukouji.com>
2015-06-27 17:42:15 +0900

    微調整　レストラン情報を削除したときにエラーになる不具合を解消

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -38,7 +38,7 @@ class RestaurantsController < ApplicationController
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
-    redirect_to restaurant_url
+    redirect_to action: 'index'
   end
 
 
