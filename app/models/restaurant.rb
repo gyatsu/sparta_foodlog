4962015bee374f3003374a8c25db9f0117f87fcf
MR.Y <programming@yatsukouji.com>
2015-06-27 16:40:12 +0900

    restaurantモデルにvalidateを追加。無記入禁止。

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ActiveRecord::Base
+  validates :name,          presence: true
+  validates :tel,           presence: true, length: { maximum: 15 }
+  validates :address,       presence: true
+  validates :url,           presence: true
 end
